8dd0848ace92e9cef8503ddf79ed953fd45871c8
tricky <517672932@qq.com>
2019-08-14 16:34:42 +0800

    添加了gulpfile注释

--- gulpfile.js
+++ gulpfile.js
@@ -18,21 +18,17 @@ const styleDist = "public/static/styles";
 // js输出的路径
 const javaScriptDist = "public/static/js";
 
-function defaultTask(cb) {
-    // place code for your default task here
-    console.log("hello world");
-    cb();
-}
+// 构建 less
 function buildLess(cb){
     // console.log("building less...");
     // cb();
     return src(stylePath)
-            .pipe(less())
-            .pipe(csso())
+            .pipe(less()) // less -> css
+            .pipe(csso()) // css压缩
             .pipe(dest(styleDist));
 }
 
-
+// 构建 js
 function buildJs(){
     // const browserify = require('browserify');
     // const source = require('vinyl-source-stream');
@@ -41,19 +37,20 @@ function buildJs(){
     // @babel/core
     console.log("正在构建新的bundle.js");
     return browserify(javaScriptPath)
-        .transform("babelify",{presets:['@babel/preset-env']})
-        .bundle()
-        .pipe(source('bundle.js'))
-        .pipe(buffer())
-        .pipe(uglify())
+        .transform("babelify",{presets:['@babel/preset-env']}) // es6 -> es5
+        .bundle() // 打包所有文件
+        .pipe(source('bundle.js')) // 给打包完的文件起个名字
+        .pipe(buffer()) // 我没讲过，但是这里的作用是将虚拟文件变成 buffer 格式，要不然下面的uglify会理解不了
+        .pipe(uglify()) // js 压缩/丑化
         .pipe(dest(javaScriptDist));
 }
 
+// 监控所有文件的变化，一旦文件发生变更，自己执行相关任务
 function watchFiles(){
     watch('src/js/**/*.js',buildJs);
     watch('src/styles/**/*.less',buildLess);
 }
-exports.default = defaultTask;
+
 exports.buildLess = buildLess;
 exports.buildJs = buildJs;
 exports.watch = watchFiles;
